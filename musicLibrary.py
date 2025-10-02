55dfd12808a19c83dd4e0624451a00cac0e17b9f
Khushi Singh <ks9696086698@gmail.com>
2025-10-02 11:47:55 +0530

    Add  musicLibrary

---
+++ musicLibrary.py
@@ -0,0 +1,14 @@
+#musiclibrary
+music={
+        "lollipop":"https://www.youtube.com/watch?v=Gr8G_ldltDE",
+        "dhani ho sab dhan":"https://www.youtube.com/watch?v=fk_VS8Y6jOA&list=RDGMEM916WJxafRUGgOvd6dVJkeQ&start_radio=1&rv=Gr8G_ldltDE",
+        "laal ghaghra":"https://www.youtube.com/watch?v=qZId59qml_4&list=RDGMEM916WJxafRUGgOvd6dVJkeQ&index=8",
+    
+    
+        "calm down":"https://open.spotify.com/album/2b2GHWESCWEuHiCZ2Skedp?si=5YvoTXEjRHqty_ii-3VQZA",
+        "baby":"https://open.spotify.com/track/6epn3r7S14KUqlReYr77hA?si=2bdf293ed721445b",
+        "waka waka":"https://open.spotify.com/track/2Cd9iWfcOpGDHLz6tVA3G4?si=03c236dff1374817"
+    
+        ,"teri meri":"https://open.spotify.com/track/1HT0RzPuChHC1kWbIplHyw?si=5019ba74ef1a42ca",
+        "sajna ve sajna":"https://open.spotify.com/track/2EFi7ey45Za7sxgRZWdmWd?si=6b9b79797b9b4701"
+        }
